cc1103c1ab7129ba1504514ec643e6835580cdc9
Joseph Abrahamson <me@jspha.com>
2014-03-05 16:05:06 -0500

    Added two unit tests demonstrating Parser behavior

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,8 +1,17 @@
 
+import           Control.Monad
+import qualified System.Environment.Parser as Env
 import           Test.Hspec
+import           Test.Hspec.Expectations
 
 main :: IO ()
 main = hspec $ do
-  describe "Nothing, nothing at all" $ do
-    it "exists" pending
+  describe "Parsing" $ do
+    it "should be able to find the \"PATH\" variable" $ do
+      void (Env.parse (Env.get "PATH")) `shouldReturn` ()
+
+    -- This test is unstable since we'd like to eliminate the
+    -- error-throwing failure API, but for now here it is.
+    it "should not be able to find the \"da39a3ee5e6b4b0d3255bfef95601890afd80709\" variable" $ do
+      Env.parse (Env.get "da39a3ee5e6b4b0d3255bfef95601890afd80709") `shouldThrow` anyIOException
 
