4622cd9eafcae6eff7aa35eabc9d72b3ec6bd8b5
Joseph Abrahamson <me@jspha.com>
2014-03-05 14:58:01 -0500

    Added Haddock comments to **/Parser.hs
    Haddock is Haskell's (quite nice) integrated documentation system. It's used
    to automatically generate documentation for all of Hackage and it's often
    much nicer to read than plain code---better formatting, hyperlinks, the
    whole nine yards.
    
    Documentation is fascinating in Haskell since so much information can be
    placed into the type signature. Often I try to only document the intent of a
    function and let the type signature show you how to use it. More complex
    signatures might deserve some doctests or specializations, but that probably
    won't occur in as simple a library as this.

--- src/System/Environment/Parser.hs
+++ src/System/Environment/Parser.hs
@@ -1,8 +1,21 @@
 
+-- |
+-- Module      :  System.Environment.Parser
+-- Copyright   :  (C) 2014 Joseph Abrahamson
+-- License     :  BSD3
+-- Maintainer  :  Joseph Abrahamson <me@jspha.com>
+-- Stability   :  experimental
+-- Portability :  non-portable
+--
+
 module System.Environment.Parser where
 
+-- | The 'Parser' type is an effectual context where you can use ENV
+-- information to build a value containing configuration information.
 data Parser a
 instance Monad Parser
 
+-- | Look up a key in the ENV. This operation may fail if the key is
+-- missing.
 get :: String -> Parser String
 get = undefined
