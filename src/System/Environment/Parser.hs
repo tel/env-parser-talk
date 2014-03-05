8315d9eb15591e14e09f54e74f8c7825bd2e55f2
Joseph Abrahamson <me@jspha.com>
2014-03-05 17:09:14 -0500

    Implemented the new parser such that the tests pass
    Also provided some explicit documentation for the `parse` function.

--- src/System/Environment/Parser.hs
+++ src/System/Environment/Parser.hs
@@ -15,16 +15,27 @@ import           System.Environment
 
 -- | The 'Parser' type is an effectual context where you can use ENV
 -- information to build a value containing configuration information.
-newtype Parser a = Parser (IO a)
-instance Monad Parser
+newtype Parser a = Parser ( IO (Either String a) )
+
+instance Monad Parser where
+  return a = Parser (return (Right a))
+  Parser ioa >>= f = Parser $ do
+    ea <- ioa
+    case ea of
+      Left err -> return $ Left err
+      Right a  -> parse  $ f a
 
 -- | Runs a 'Parser' in the 'IO' monad, looking up the required environment
 -- variables and using them to build the final value. In the event that
 -- lookup fails this reports the name of the missing variable.
 parse :: Parser a -> IO (Either String a)
-parse = undefined
+parse (Parser ioe) = ioe
 
 -- | Look up a key in the ENV. This operation may fail if the key is
 -- missing.
 get :: String -> Parser String
-get s = Parser (getEnv s)
+get s = Parser $ do
+  ma <- lookupEnv s
+  return $ case ma of
+    Nothing -> Left s
+    Just a  -> Right a
