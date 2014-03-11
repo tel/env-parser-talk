b59c8a93276033199f10d97959497adcc16e8fee
Joseph Abrahamson <me@jspha.com>
2014-03-11 17:23:49 -0400

    Slap-dash first attempt to get some more parsers
    This attempt brings up a fairly large number of issues:
    
    1.     fromEnv :: String -> Either String a
    
       is too weak to use recursively, we need to keep a parser
       of some kind or another around
    
       maybe something simple like
    
           fromEnv :: Atto.Parser a
    
       is enough.
    
    2. we should pick an appropriate input format for Word and Int types
       (probably hexadecimal, but it should be done correctly)
    
    3. methods of roundtripping through the parser should be tested---
       we don't have a genuine printer module (that'd be useless here)
       so it's a little challenging to know how we're building the tests
    
    4. Tuples rely on better recursivity in the parser

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -4,13 +4,23 @@ import           Control.Applicative
 import           Control.Monad
 import qualified Data.ByteString             as S
 import qualified Data.ByteString.Char8       as S8
+import qualified Data.ByteString.Lazy        as Sl
+import qualified Data.CaseInsensitive        as Ci
+import           Data.Int
+import           Data.Ratio
+import           Data.String
+import qualified Data.Text                   as T
+import qualified Data.Text.Encoding          as Te
+import qualified Data.Text.Lazy              as Tl
+import qualified Data.Text.Lazy.Encoding     as Tle
+import           Data.Word
 import qualified System.Environment.Parser   as Env
 import           System.Posix.Env.ByteString
+import           Test.Helpers
 import           Test.Hspec
 import           Test.Hspec.Expectations
 import           Test.QuickCheck
 import           Test.QuickCheck.Monadic
-import           Data.String
 
 -- | Generate a gibberish, @[a-zA-Z]@, and non-empty \"name\" of
 -- minimally-bounded length.
@@ -23,6 +33,20 @@ arbitraryNameBS = fmap S8.pack . arbitraryName
 main :: IO ()
 main = hspec $ do
 
+  describe "Utilities" $ do
+    describe "FreeSNR" $ do
+      context "Properties" $ do
+        it "0 + x == x" pending
+        it "x + 0 == x" pending
+        it "1 * x == x" pending
+        it "x * 1 == x" pending
+        it "0 * x == 0" pending
+        it "x * 0 == 0" pending
+        it "(x + y)z == xz + yz" pending
+    describe "Collect" $ do
+      context "Properties" $ do
+        it "monoid laws" pending
+
   -- Parsing is the action of actually pulling information from the ENV
   -- dictionary (or something like it) in order to build the type
   -- contextually wrapped in the 'Env.Parser'.
@@ -34,7 +58,7 @@ main = hspec $ do
     context "Mocked" $ do
       context "it should successfully parse when" $ do
         it "the environment is complete" pending
-        it "the environment is incomplete, but missing values have defaults" 
+        it "the environment is incomplete, but missing values have defaults"
           pending
       context "it should fail to parse when" $ do
         it "missing some set of variables without defaults" pending
@@ -51,10 +75,9 @@ main = hspec $ do
   -- such as all of the environment variables required to successfully
   -- gather the needed information and the documentation around each one.
   describe "Documentation" $ do
-    it "should have tests" 
+    it "should have tests"
       $ pendingWith "documentation interface design/alternative instance"
     it "should show defaults properly" pending
-
 
   -- Deserialization is "parsing", except that we already used that term.
   -- In particular, deserialization means taking a ByteString from the
@@ -62,33 +85,36 @@ main = hspec $ do
   -- mostly describes the actions of built-in FromEnv instances.
   describe "Deserialization" $ do
     describe "FromEnv" $ do
-      it "should have tests" 
+      it "should have tests"
         $ pendingWith "descriptions of intended behaviors"
 
       describe "String-alikes" $ do
-        it "String"          pending
-        it "ByteString"      pending
-        it "Lazy.ByteString" pending
-        it "Text"            pending
-        it "Lazy.Text"       pending
+        describe "Roundtrips" $ do
+          it "String"          $ rt id strUtf8
+          it "ByteString"      $ rt strUtf8 id
+          it "Lazy.ByteString" $ rt strUtf8l Sl.toStrict
+          it "Text"            $ rt T.pack Te.encodeUtf8
+          it "Lazy.Text"       $ rt Tl.pack (Sl.toStrict . Tle.encodeUtf8)
 
       describe "Number-alikes" $ do
-        it "Int"               pending
-        it "Integer"           pending
-        it "Int8"              pending
-        it "Int16"             pending
-        it "Int32"             pending
-        it "Int64"             pending
-        it "Double"            pending
-        it "Float"             pending
-        it "Scientific"        pending
-        it "Attoparsec.Number" pending
-        it "Ratio Int"         pending
-        it "Ratio Integer"     pending
-        it "Ratio Int8"        pending
-        it "Ratio Int16"       pending
-        it "Ratio Int32"       pending
-        it "Ratio Int64"       pending
+        describe "Roundtrips" $ do
+          it "Int"           $ rti (P      :: P Int)               utf8
+          it "Integer"       $ rti (P      :: P Integer)           utf8
+          it "Int8"          $ rti (P      :: P Int8)              utf8
+          it "Int16"         $ rti (P      :: P Int16)             utf8
+          it "Int32"         $ rti (P      :: P Int32)             utf8
+          it "Int64"         $ rti (P      :: P Int64)             utf8
+          it "Double"        $ rti (P      :: P Double)            utf8
+          it "Float"         $ rti (P      :: P Float)             utf8
+          it "Ratio Int"     $ rt' (ratGen :: Gen (Ratio Int))     prRatio
+          it "Ratio Integer" $ rt' (ratGen :: Gen (Ratio Integer)) prRatio
+          -- it "Ratio Int8"    $ rt' (ratGen :: Gen (Ratio Int8))    prRatio
+          it "Ratio Int8"    $ pendingWith "fix overflow errors"
+          it "Ratio Int16"   $ rt' (ratGen :: Gen (Ratio Int16))   prRatio
+          it "Ratio Int32"   $ rt' (ratGen :: Gen (Ratio Int32))   prRatio
+          it "Ratio Int64"   $ rt' (ratGen :: Gen (Ratio Int64))   prRatio
+          it "Scientific"    $ pendingWith "How to display Scientific"
+          it "Atto.Number"   $ pendingWith "How to display Atto.Number"
 
       describe "Time types" $ do
         describe "UTCTime formats" $ do
@@ -100,16 +126,41 @@ main = hspec $ do
         it "ZonedTime"                  pending
 
       describe "Bits" $ do
-        it "Word"   pending
-        it "Word8"  pending
-        it "Word16" pending
-        it "Word32" pending
-        it "Word64" pending
+        describe "Numeric parse" $ do
+          describe "Roundtrips" $ do
+            it "Word"   pending
+            it "Word8"  pending
+            it "Word16" pending
+            it "Word32" pending
+            it "Word64" pending
+        describe "Hex parse" $ do
+          describe "Roundtrips" $ do
+            it "Word"   pending
+            it "Word8"  pending
+            it "Word16" pending
+            it "Word32" pending
+            it "Word64" pending
+          describe "Units" $ do
+            it "should parse the right number of digits" $
+              Env.fromEnv "0x10" `shouldBe` (Right (16 :: Word8))
+            it "should not truncate parses" $
+              let isLeft e = case e of { Left _ -> True; _ -> False }
+              in (Env.fromEnv "0x0fff" :: Either String Word8)
+                   `shouldSatisfy` isLeft
 
       describe "Default types" $ do
-        it "()"   pending
-        it "Bool" pending
-        it "Char" pending
+        context "()" $ do
+          it "should always succeed" $ property $ \s ->
+            Env.fromEnv (S8.pack s) == Right ()
+        context "Bool" $ do
+          context "total lexical space (case insensitive" $ do
+            it "true"  $ Env.fromEnv "true"  == Right True
+            it "yes"   $ Env.fromEnv "yes"   == Right True
+            it "1"     $ Env.fromEnv "1"     == Right True
+            it "no"    $ Env.fromEnv "no"    == Right False
+            it "false" $ Env.fromEnv "false" == Right False
+            it "0"     $ Env.fromEnv "0"     == Right False
+        it "Char" $ pendingWith "is this even a meaningful instance?"
 
       describe "Special types" $ do
         it "System.ExitCode" pending
@@ -128,24 +179,33 @@ main = hspec $ do
           it "FileMode"       pending
           it "Limit"          pending
 
+      -- The current fromEnv :: S.ByteString -> Either String a
+      -- interace is too weak to make this work out... Hm.
       describe "Lists and Sets" $ do
         it "FromEnv a => [a]"   pending
         it "IntSet"             pending
         it "FromEnv a => Set a" pending
 
       describe "Modifiers" $ do
-        it "FromEnv a => Maybe a" 
+        it "FromEnv a => Maybe a"
           $ pendingWith "define semantics: must exist, may fail parse?"
         it "FromEnv a, b => Either a b"
           $ pendingWith "define semantics"
         context "CI" $ do
-          it "Char"              pending
-          it "String"            pending
-          it "ByteString"        pending
-          it "Lazy.ByteString"   pending
-          it "Text"              pending
-          it "Lazy.Text"         pending
-          it "FoldText a => [a]" pending
+          describe "Roundtrips" $ do
+            it "String"
+              $ rt (Ci.mk :: String -> Ci.CI String) 
+                   (strUtf8 . Ci.foldedCase)
+            it "ByteString"
+              $ rt (Ci.mk . strUtf8) Ci.foldedCase
+            it "Lazy.ByteString"
+              $ rt (Ci.mk . strUtf8l) (Sl.toStrict . Ci.foldedCase)
+            it "Text"
+              $ rt (Ci.mk . T.pack) (Te.encodeUtf8 . Ci.foldedCase)
+            it "Lazy.Text"
+              $ rt (Ci.mk . Tl.pack) (  Sl.toStrict 
+                                      . Tle.encodeUtf8
+                                      . Ci.foldedCase  )
 
       describe "Network Typs" $ do
         describe "Network.BSD" $ do
